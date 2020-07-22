819b67a7a8fbf217f74d43db1dcafba835aceb44
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2020-07-23 00:42:22 +0200

    Prepare usage of composer/npm

--- README.md
+++ README.md
@@ -4,3 +4,31 @@ Joomla system plugin to register and override JHtml helpers. And more.
 Plugin for ghsvs.de templates.
 
 Don't use it if you don't need it.
+
+## npm/composer. Create new release.
+
+check `package.json` and add plugin `version` and further settings like `minimumPhp` and so on. 
+
+```
+cd src
+composer install
+```
+(if not done yet and `/vendor/` doesn't exist yet)
+
+or
+
+`composer update`
+(for `/vendor/` updates)
+
+`cd ..`
+
+`npm install` (if not done yet)
+
+`node build.js`
+
+new ZIP is in `/dist/`
+
+Create release with tag.
+
+Get download link inside tag release and add to release description.
+
---
+++ build.js
@@ -0,0 +1,51 @@
+const {
+  copy,
+  exists,
+  mkdir,
+  readFile,
+  unlink: unl,
+  writeFile,
+} = require("fs-extra");
+const util = require("util");
+const rimRaf = util.promisify(require("rimraf"));
+const {
+	version,
+	minimumPhp,
+	maximumPhp,
+	minimumJoomla,
+	maximumJoomla,
+	allowDowngrades,
+} = require("./package.json");
+
+(async function exec()
+{
+	// Remove old folders.
+  await rimRaf("./dist");
+  await rimRaf("./package");
+	
+	// Copy and create new work dir.
+  await copy("./src", "./package");
+
+	// Create new dist dir.
+  if (!(await exists("./dist"))) {
+    await mkdir("./dist");
+  }
+
+  let xml = await readFile("./package/bs3ghsvs.xml", { encoding: "utf8" });
+  xml = xml.replace(/{{version}}/g, version);
+	xml = xml.replace(/{{minimumPhp}}/g, minimumPhp);
+	xml = xml.replace(/{{maximumPhp}}/g, maximumPhp);
+	xml = xml.replace(/{{minimumJoomla}}/g, minimumJoomla);
+	xml = xml.replace(/{{maximumJoomla}}/g, maximumJoomla);
+	xml = xml.replace(/{{allowDowngrades}}/g, allowDowngrades);
+
+  await writeFile("./package/bs3ghsvs.xml", xml, { encoding: "utf8" });
+  await unl("./package/composer.json");
+  await unl("./package/composer.lock");
+
+  // Package it
+  const zip = new (require("adm-zip"))();
+  zip.addLocalFolder("package", false);
+  zip.writeZip(`dist/plg_system_bs3ghsvs-${version}.zip`);
+
+})();
