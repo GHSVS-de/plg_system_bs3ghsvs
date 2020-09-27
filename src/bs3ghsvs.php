5b8bad457f27d2c92a1897ed2efc2fcc20bfaf06
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2020-09-27 23:51:38 +0200

    See commit description 2020.09.27

--- src/bs3ghsvs.php
+++ src/bs3ghsvs.php
@@ -983,107 +983,8 @@ class PlgSystemBS3Ghsvs extends CMSPlugin
 			&& $context === 'com_content.article'
 			&& strpos($article->text, '{pagebreakghsvs-slider ') !== false
 		){
-			$regex = '#<p[^>]*>\s*{pagebreakghsvs-slider\s+(.+?)}\s*</p>#i';
-			$regexEnd = '#<p[^>]*>\s*{pagebreakghsvs-slider[^}]*slidersEnd[^}]*}\s*</p>#iU';
-			$headingTagGhsvs = $this->params->get('headingTagGhsvs', 'h4');
-			$collector = $endedTextBlocks = array();
-
-			// Array mit mindestens 1 Text-Element, egal, ob slidersEnd gefunden oder nicht.
-			$endedTextBlocks = preg_split($regexEnd, $article->text);
-			
-			// Finde falsch platzierte Eingaben von slidersEnd.
-			foreach ($endedTextBlocks as $i => $endedText)
-			{
-				if (!trim($endedText))
-				{
-					unset($endedTextBlocks[$i]);
-				}
-			}
-			
-			// Now find embedded slides in each block.
-			foreach ($endedTextBlocks as $i => $endedText)
-			{
-				preg_match_all($regex, $endedText, $matches, PREG_SET_ORDER);
-				$text = preg_split($regex, $endedText);
-
-				// Teil vor erstem slide, der aber ggf. auch leer sein kann,
-				//  falls erstes regex ganz am Anfang im Beitrag.
-				$collector[] = $text[0];
-				
-				// Es wurden Panel-Regex gefunden. Dann Accordion aufbauen
-				if (count($text) > 1)
-				{
-					$selector = $dataParent = 'pagebreakghsvs' . $article->id . '-' . $i;
-					
-					if (!$print && !$isRobot)
-					{
-						$collector[] = HTMLHelper::_('bootstrap.startAccordion',
-								$selector,
-								array(
-									// Damit mehrere geöffnet werden können auf FALSE!
-									'parent' => false,
-								)
-							);
-					}
-
-					// Panels.
-					foreach ($text as $key => $subtext)
-					{
-						if ($key)
-						{
-							$match = $matches[$key - 1];
-							$match = (array) Utility::parseAttributes($match[0]);
-							$title = $title2 = $class = '';
-							
-							$title = htmlspecialchars_decode($match['title'], ENT_COMPAT);
-							$title = htmlspecialchars($title, ENT_COMPAT, 'utf-8');
-							$title2 = @htmlspecialchars_decode($match['title2'], ENT_COMPAT);
-							$title2 = htmlspecialchars($title2, ENT_COMPAT, 'utf-8');
-	
-							$href = $selector . '-' . $key;
-							
-							if (!$print && !$isRobot)
-							{
-								$collector[] = HTMLHelper::_('bootstrap.addSlide',
-											$selector,
-											$title,
-											$href,
-											$class,
-											$headingTagGhsvs,
-											$title2
-										);
-							}
-							else
-							{
-								// $collector[] = '<' . $headingTagGhsvs . '>';
-								// $collector[] = $title . ($title2 ? ' - ' . $title2 : '');
-								// $collector[] = '</' . $headingTagGhsvs . '>';
-							}
-							
-							$collector[] = '<h4 class="headAutoByPagebrekghsvs">' . $title . ($title2 ? ' - ' . $title2 : '') . '</h4>';
-
-							$collector[] = $subtext;
-							
-							if (!$print && !$isRobot)
-							{
-								$collector[] = HTMLHelper::_('bootstrap.endSlide');
-							}
-							$collector[] = "\n<!--endSlide-->\n";
-						}
-					}
-
-					if (!$print && !$isRobot)
-					{
-						$collector[] = HTMLHelper::_('bootstrap.endAccordion');
-
-					// Aktive IDs in Session schreiben mit Ajay-Plugin.
-					$collector[] = HTMLHelper::_('bs3ghsvs.activeToSession', $dataParent);
-					}
-					
-					$collector[] = "\n<!--endAccordion-->\n";
-				}
-			}
-			$article->text = implode("\n", $collector);
+			JLoader::register('Bs3ghsvsPagebreak', JPATH_PLUGINS . '/system/bs3ghsvs/Helper/PagebreakHelper.php');
+			Bs3ghsvsPagebreak::buildSliders($article->text, $article->id);
 		}
 		###### pagebreakghsvs-slider - END
 	}
@@ -1139,7 +1040,7 @@ class PlgSystemBS3Ghsvs extends CMSPlugin
 		){
 			$doc = Factory::getDocument();
 			$doc->addCustomTag('<meta property="og:title" content="' . htmlentities($doc->getTitle(), ENT_QUOTES, 'utf-8') . '">');
-			$doc->addCustomTag('<meta property="og:url" content="' . $doc->getBase() . '">');
+			$doc->addCustomTag('<meta property="og:url" content="' . Uri::current() . '">');
 			$doc->addCustomTag('<meta property="og:site_name" content="' . htmlentities($this->app->get('sitename'), ENT_QUOTES, 'utf-8') . '">');
 			$doc->addCustomTag('<meta property="og:description" content="' . htmlentities($doc->getDescription(), ENT_QUOTES, 'utf-8') . '">');
 			
