ce2fcc99951e92d44ab873bc7a75f3bbbac62f07
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-01-23 13:51:30 +0100

    PHP 8 (Deprecated and some camelcase vars). StringHelper:: instead of String::.CS

--- src/Helper/ArticleHelper.php
+++ src/Helper/ArticleHelper.php
@@ -29,7 +29,7 @@ JModelLegacy::addIncludePath($com_path . '/models', 'ContentModel');
 abstract class Bs3ghsvsArticle
 {
 	protected static $loaded = array();
-	
+
 	/**
 	 * E.g. for blog <> list view with footables.
 	 */
@@ -367,7 +367,7 @@ abstract class Bs3ghsvsArticle
 	 *
 	 * @since   1.6
 	 */
-	public static function groupBy($list, $fieldName, $article_grouping_direction, $fieldNameToKeep = null)
+	public static function groupBy($list, $fieldName, $direction, $fieldNameToKeep = null)
 	{
 		$grouped = array();
 
@@ -388,7 +388,7 @@ abstract class Bs3ghsvsArticle
 				$grouped[$item->$fieldName] = array();
 			}
 
-			if (is_null($fieldNameToKeep))
+			if ($fieldNameToKeep === null)
 			{
 				$grouped[$item->$fieldName][$key] = $item;
 			}
@@ -400,7 +400,7 @@ abstract class Bs3ghsvsArticle
 			unset($list[$key]);
 		}
 
-		$article_grouping_direction($grouped);
+		$direction($grouped);
 
 		return $grouped;
 	}
@@ -408,16 +408,17 @@ abstract class Bs3ghsvsArticle
 	/**
 	 * Groups items by date
 	 *
-	 * @param   array   $list                        list of items
-	 * @param   string  $type                        type of grouping
-	 * @param   string  $article_grouping_direction  ordering direction
-	 * @param   string  $month_year_format           date format to use
+	 * @param   array   $list             list of items
+	 * @param   string  $type             type of grouping
+	 * @param   string  $direction        ordering direction
+	 * @param   string  $monthYearFormat  date format to use
+	 * @param   string  $field            date field to group by
 	 *
 	 * @return  array
 	 *
 	 * @since   1.6
 	 */
-	public static function groupByDate($list, $type = 'year', $article_grouping_direction, $month_year_format = 'F Y')
+	public static function groupByDate($list, $type = 'year', $direction = 'ksort', $monthYearFormat = 'F Y', $field = 'created')
 	{
 		$grouped = array();
 
@@ -436,7 +437,7 @@ abstract class Bs3ghsvsArticle
 			switch ($type)
 			{
 				case 'month_year' :
-					$month_year = String::substr($item->created, 0, 7);
+					$month_year = StringHelper::substr($item->created, 0, 7);
 
 					if (!isset($grouped[$month_year]))
 					{
@@ -448,7 +449,7 @@ abstract class Bs3ghsvsArticle
 
 				case 'year' :
 				default:
-					$year = String::substr($item->created, 0, 4);
+					$year = StringHelper::substr($item->created, 0, 4);
 
 					if (!isset($grouped[$year]))
 					{
@@ -462,14 +463,14 @@ abstract class Bs3ghsvsArticle
 			unset($list[$key]);
 		}
 
-		$article_grouping_direction($grouped);
+		$direction($grouped);
 
 		if ($type === 'month_year')
 		{
 			foreach ($grouped as $group => $items)
 			{
 				$date                      = new Date($group);
-				$formatted_group           = $date->format($month_year_format);
+				$formatted_group           = $date->format($monthYearFormat);
 				$grouped[$formatted_group] = $items;
 
 				unset($grouped[$group]);
@@ -495,14 +496,14 @@ abstract class Bs3ghsvsArticle
 
 		$keys = \array_flip($keys);
 		ksort($keys);
-		
+
 		$sig = md5(serialize(array($articleId, $keys, $asArray)));
-		
+
 		if (isset(static::$loaded[__METHOD__][$sig]))
 		{
 			return static::$loaded[__METHOD__][$sig];
 		}
-		
+
 		$prefix = 'article';
 		$activeXml = Bs3GhsvsFormHelper::getActiveXml(
 			$prefix,
@@ -525,7 +526,7 @@ abstract class Bs3ghsvsArticle
 				unset($activeXml[$key]);
 				continue;
 			}
-			
+
 			$activeXml[$key] = $keyValue;
 		}
 
@@ -553,7 +554,7 @@ abstract class Bs3ghsvsArticle
 		foreach ($result as $key => $value)
 		{
 			$result[$key] = json_decode($value->value, true);
-			
+
 			if (json_last_error() !== JSON_ERROR_NONE)
 			{
 				$result[$key] = $value->value;
@@ -561,14 +562,14 @@ abstract class Bs3ghsvsArticle
 		}
 
 		if ($asArray === false)
-		{ 
+		{
 			$result = new Registry($result);
 		}
 		static::$loaded[__METHOD__][$sig] = $result;
-		
+
 		return $result;
 	}
-	
+
 	public static function getExtensionData(int $articleId)
 	{
 		$extensionData = self::getExtraFields($articleId, array('extension'));
@@ -582,7 +583,7 @@ abstract class Bs3ghsvsArticle
 
 		return $extensionData->get('extension', new stdClass, 'OBJECT');
 	}
-	
+
 	public static function getVariousData(int $articleId)
 	{
 		$data = self::getExtraFields($articleId, array('various'));
@@ -600,16 +601,16 @@ abstract class Bs3ghsvsArticle
 	public static function getJcfieldsAsRegistry($item) : Registry
 	{
 		$JcfieldsAsRegistry = new Registry();
-		
+
 		if (!empty($item->jcfields) && is_array($item->jcfields))
 		{
 			$jcfields = array();
 
 			foreach ($item->jcfields as $jcfield)
 			{
-				$jcfields[$jcfield->name] = $jcfield->rawvalue; 
+				$jcfields[$jcfield->name] = $jcfield->rawvalue;
 			}
-			
+
 			$JcfieldsAsRegistry = new Registry($jcfields);
 		}
 
@@ -619,7 +620,7 @@ abstract class Bs3ghsvsArticle
 	public static function buildFlagImages(array $languages)
 	{
 		$flags = \array_flip($languages);
-		
+
 		foreach ($flags as $flag => $key)
 		{
 			$alt = Text::sprintf('PLG_SYSTEM_BS3GHSVS_EXTENSION_LANGUAGE_X', str_replace('_', '-', $flag));
@@ -632,7 +633,7 @@ abstract class Bs3ghsvsArticle
 		}
 		return $flags;
 	}
-	
+
 	/**
 	 * Check if it's worth to go on with 'extension' datas on display actions.
 	 */
@@ -655,7 +656,7 @@ abstract class Bs3ghsvsArticle
 		return false;
 	}
 	/**
-	 * 
+	 *
 	 */
 	public static function isVariousEmpty($various)
 	{
@@ -663,14 +664,14 @@ abstract class Bs3ghsvsArticle
 		{
 			return true;
 		}
-		
+
 		// Nearly impossible that this property exists AND is set to 0.
 		// If it exists it should be alwys 1.
 		if (empty($various->bs3ghsvs_various_active))
 		{
 			return true;
 		}
-		
+
 		foreach ($various as $key => $value)
 		{
 			if (!empty($value))
@@ -688,7 +689,7 @@ abstract class Bs3ghsvsArticle
 	public static function buildUniqueIdFromJinput($prefix = '')
 	{
 		$jinput = Factory::getApplication()->input;
-		
+
 		$getThis = array(
 			'Itemid',
 			'option',
@@ -697,21 +698,21 @@ abstract class Bs3ghsvsArticle
 			'id',
 			'task'
 		);
-		
+
 		$id = '';
-		
+
 		foreach ($getThis as $GetMe)
 		{
 			$id .= '-' . $jinput->get($GetMe);
 		}
-		
+
 		$id = $prefix . '-' . ApplicationHelper::stringURLSafe(base64_encode($id));
-		
+
 		return $id;
 	}
 
 ###################################### 	 * NUR FÜR Datenbank-Migration! REMOVE LATER - START
-	
+
 	/**
 	 * NUR FÜR Datenbank-Migration!
 	 * Zum Konvertieren alter extension Felder u-ä. nach neue Plugin-Anfordernisse,
@@ -732,16 +733,16 @@ abstract class Bs3ghsvsArticle
 		{
 			$registry = new Registry($article->attribs);
 			$params = $registry->toArray();
-			
+
 			$extensionData = array();
 			$various = array();
-			
+
 			$various['articlesubtitle'] = isset($params['articlesubtitle1'])
 				? $params['articlesubtitle1'] : '';
 
 			$extensionData['name'] = isset($params['extensionname'])
 				? $params['extensionname'] : '';
-			
+
 			$extensionData['description'] = isset($params['extensiondescription'])
 				? $params['extensiondescription'] : '';
 
@@ -768,9 +769,9 @@ abstract class Bs3ghsvsArticle
 
 			$extensionData['history'] = isset($params['extensionhistory'])
 				? $params['extensionhistory'] : '';
-			
+
 			$extensionData['updateserver'] = '';
-			
+
 			if (isset($params['extensionupdateserver']))
 			{
 				if (!$params['extensionupdateserver'])
@@ -782,7 +783,7 @@ abstract class Bs3ghsvsArticle
 					$extensionData['updateserver'] = (string) $params['extensionupdateserver'];
 				}
 			}
-			
+
 			/*unset(
 				$params['extensionversion'],
 				$params['extensiontest'],
@@ -800,7 +801,7 @@ abstract class Bs3ghsvsArticle
 					}
 				}
 			}*/
-			
+
 			$query = $db->getQuery(true)
 				->delete($db->qn('#__bs3ghsvs_article'))
 				->where($db->qn('article_id') . ' = ' . $article->id)
@@ -811,10 +812,10 @@ abstract class Bs3ghsvsArticle
 #
 			$db->setQuery($query);
 			$db->execute();
-			
+
 			$extensionEmpty = $variousEmpty = true;
 			$tuples = array();
-			
+
 			// All required fields present? Otherwise skip.
 			if (!self::isExtensionEmpty((object) $extensionData))
 			{
@@ -823,10 +824,10 @@ abstract class Bs3ghsvsArticle
 							. ', ' . $db->q('extension')
 							. ', ' . $db->q(json_encode($extensionData))
 				;
-				
+
 				$extensionEmpty = false;
 			}
-			
+
 			foreach ($various as $checkValue)
 			{
 				if (!empty($checkValue))
@@ -835,7 +836,7 @@ abstract class Bs3ghsvsArticle
 					break;
 				}
 			}
-			
+
 			if ($variousEmpty === false)
 			{
 				$various['bs3ghsvs_various_active'] = 1;
@@ -857,7 +858,7 @@ abstract class Bs3ghsvsArticle
 			}
 		}
 	}
-	
+
 	/**
 	 * NUR FÜR Datenbank-Migration!
 	 * Zum Konvertieren alter core_params in com_tags,
@@ -881,7 +882,7 @@ abstract class Bs3ghsvsArticle
 			$db->updateObject('#__ucm_content', $article, 'core_content_id');
 		}
 	}
-	
+
 	/**
 	 * NUR FÜR Datenbank-Migration!
 	 * Zum Konvertieren alter <hr class="system-pagebreak" title="Title" alt="Alt" />
@@ -911,7 +912,7 @@ abstract class Bs3ghsvsArticle
 				$regex = '#<hr(.*)class="system-pagebreak"(.*)\/>#iU';
 				$matches = array();
 				preg_match_all($regex, $text, $matches, PREG_SET_ORDER);
-				
+
 				foreach ($matches as $match)
 				{
 					$match[0] = trim($match[0]);
@@ -942,6 +943,6 @@ abstract class Bs3ghsvsArticle
 			}
 		}
 	}
-	
+
 ###################################### REMOVE LATER - END
 }
